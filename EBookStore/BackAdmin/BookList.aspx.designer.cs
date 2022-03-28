0a59c09f1c8796be5b4e616b3e3593c99dccfe6b
大山 李 <404383303@m365.fju.edu.tw>
2022-03-28 15:55:59 +0800

    增加上架

--- EBookStore/BackAdmin/BookList.aspx.designer.cs
+++ EBookStore/BackAdmin/BookList.aspx.designer.cs
@@ -24,6 +24,15 @@ namespace EBookStore.BackAdmin
         protected global::System.Web.UI.WebControls.Button btnCreate;
 
         /// <summary>
+        /// btnReShelf 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnReShelf;
+
+        /// <summary>
         /// btnDelete 控制項。
         /// </summary>
         /// <remarks>
